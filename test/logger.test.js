1bf330588c4194b15a5855dd7e0c34b513a1e8d0
Charlie <heinercj@gmail.com>
2018-04-09 17:10:07 -0700

    logger class set up with log method. writes to file given

---
+++ test/logger.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const fs = require('fs');
+const { promisify } = require('util');
+const unlink = promisify(require('fs').unlink);
+const Logger = require('../lib/logger/Logger');
+
+describe('Logger class', () => {
+    let logger;
+    const testLogFile = './test/logger-test-log.txt';
+    const testExpectedFile = './test/logger-expected-test.txt';
+
+    beforeEach(() => {
+        logger = new Logger(testLogFile);  
+        unlink(testLogFile)
+            .catch(err => {
+                if(err.code !== 'ENOENT') throw err; 
+            });  
+    });
+
+    it('logs message to file given', () => {
+        const message = 'Hello World';
+        logger.log(message);
+        const expected = fs.readFileSync(testExpectedFile);
+        assert.ok(expected);
+    });
+});
